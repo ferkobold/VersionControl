f30af49ea8cde4f17baba95ef290d32aa1b69470
ferkobold <barta.ferenc98@gmail.com>
2020-10-11 14:45:58 +0200

    Loading the data from Tick

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -12,9 +12,20 @@ namespace week05
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        /*A Form1 osztály szintjén példányosítsd az ORM objektumot!
+         (Ha nem írtad át, akkor PortfolioEntities 
+         néven kellett létrejöjjön.)*/
+        List<Tick> Ticks;
+        /*A Form1 osztály szintjén hozz létre egy Tick típusú elemekből
+         álló listára mutató referenciát -- Ticks*/
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+        /*A konstruktorban másold az adattáblát a memóriába.*/
+            dataGridView1.DataSource = Ticks;
+        /*Majd töltsd fel fele a DataGridViewt.*/
         }
     }
 }
