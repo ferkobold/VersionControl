9bbcbbd02ee663359c578bfbe2c25417f95bde8f
ferkobold <barta.ferenc98@gmail.com>
2020-10-25 11:53:03 +0100

    Entities/Person class

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 
 /*Enumeráció: olyan típus, amely meghatározott értékek névvel ellátott
