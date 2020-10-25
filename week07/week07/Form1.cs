dacadd5e3db88b12d6ce6d3dff9a3717129727b9
ferkobold <barta.ferenc98@gmail.com>
2020-10-25 11:48:51 +0100

    Entities folder, Gender enumeration

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+/*Enumeráció: olyan típus, amely meghatározott értékek névvel ellátott
+ halmazát képviseli. Felsorolt típust az enum kulcsszo segítségével
+ deklarálunk. Az enumeráció osztályhoz hasonló elem, ezért az osztályokkal
+ egy szinten érdemes deklarálni.*/
 namespace week07
 {
     public partial class Form1 : Form
