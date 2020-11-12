2b486a37dc04662096e41fb98f676b3231d25743
ferkobold <barta.ferenc98@gmail.com>
2020-11-12 19:46:31 +0100

    Creating Ball class

--- week08/week08/Entities/Ball.cs
+++ week08/week08/Entities/Ball.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace week08.Entities
 {
-    class Ball
+    public class Ball : Label
     {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+        //Milyen billentyűkombinációval lehet eseményt létrehozni? 
+        // += és = között az a difi, hogy += így megváltoztatja
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            //DrawImage függvényn
+            //PaintEventArgs argumentumokból kérjük le az adott osztályhoz létrehozott
+            //példányt: e.Graphics
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+            
     }
 }
