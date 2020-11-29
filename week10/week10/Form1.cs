2519a3b976845ff9d18db98d26dfe7a02e287845
ferkobold <barta.ferenc98@gmail.com>
2020-11-29 17:45:35 +0100

    Adding Generations

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -21,6 +21,7 @@ namespace week10
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        
         public Form1()
         {
             InitializeComponent();
@@ -28,25 +29,48 @@ namespace week10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.GameOver += Gc_GameOver;
-
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
-
+            gc.GameOver += Gc_GameOver;
             gc.Start();
 
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
         }
         private void Gc_GameOver(object sender)
         {
             generation++;
             label1.Text = string.Format(
                 "{0}. generáció", generation);
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                }
+                else
+                {
+                    gc.AddPlayer(b);
+                }
+
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                }
+                else
+                {
+                    gc.AddPlayer(b.Mutate());
+                }
+            }
+            gc.Start();
         }
     }
 }
